62dde4b97e9baa59c2d0bca2aaf8923341edef0d
Sharmaishika <Ishika.2001rma@gmail.com>
2023-12-29 16:32:54 +0530

    sum of integers

---
+++ integers.c
@@ -0,0 +1,14 @@
+  // sum of integers
+#include<stdio.h>
+int main(){          //main function
+    int n,m,sum=0;       //take integers  
+    printf("Enter a number:");
+    scanf("%d",&n);
+    
+    printf("Enter a number:");
+    scanf("%d",&m);
+    
+    sum= m+n;               //adding of integers
+    printf("sum is =%d",sum);
+    return 0;
+}
