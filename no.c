2683aaab4916d4f82ea8bb0e2c79a79ce7ccf669
Sharmaishika <Ishika.2001rma@gmail.com>
2024-01-07 15:04:25 +0530

    sumin of numbers

---
+++ no.c
@@ -0,0 +1,14 @@
+ //sum of numbers
+ #include<stdio.h>
+ int main(){
+    int n,m,p,sum=0;
+    printf("enter a number:");
+    scanf("%d",&n);
+    printf("enter a number:");
+    scanf("%d",&m);
+    printf("enter a number:");
+    scanf("%d",&p);
+    sum=n+m+p;
+    printf("sum is = %d",sum);
+    return 0;
+ }
