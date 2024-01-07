f1df62f1a40ee07e95c79d389df320b74e5ba0bb
Sharmaishika <Ishika.2001rma@gmail.com>
2024-01-07 15:28:44 +0530

    driving license

---
+++ nested.c
@@ -0,0 +1,18 @@
+                   //nested if
+#include<stdio.h>
+void main(){
+    int age=20,dl=1;
+    if(age>18){
+        if(dl ==1){
+            printf("Drive");
+        }else{
+        printf("Get your driving license");
+    }
+        
+    }
+    else{ 
+        printf("You are not elegible");
+}
+    
+}
+
