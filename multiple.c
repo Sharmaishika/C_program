cf248ed5ef6de01f237783fe6dde740491f0b94c
Sharmaishika <Ishika.2001rma@gmail.com>
2024-01-16 15:03:56 +0530

    multiple of 5 or not

---
+++ multiple.c
@@ -0,0 +1,14 @@
+        //it is multiple of 5 or not
+#include<stdio.h>
+void main(){
+    
+    int number=25;
+    if(number%5==0){
+        //falls here if the condition is true
+        printf("yes it is an multiple of 5");
+    }
+    else{
+        // if the condition is false
+        printf("its not a multiple of 5");
+    }
+}
