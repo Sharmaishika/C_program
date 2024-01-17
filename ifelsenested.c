0f59203b9634f906adadb8ea012b639c8dbe17be
Sharmaishika <Ishika.2001rma@gmail.com>
2024-01-17 16:34:16 +0530

    if else of voting

---
+++ ifelsenested.c
@@ -0,0 +1,16 @@
+     //if else nested
+#include<stdio.h>
+void main(){
+    int age=18;char ans='y';
+    if(age>=18){
+        if(ans=='y'){
+            printf("you can vote");
+        }
+        else{
+            printf("get your voter id");
+        }}
+        else{
+            printf("you are not elegible");
+        }
+    
+}
